812b3702392ba19e55e761a2567b04be785dd562
Yegor Yellow <wellyellowed@gmail.com>
2020-11-18 19:39:45 +0200

    - add mods: psi, industrialforegoing, undergarden with initial configurations

--- server/download_mods.sh
+++ server/download_mods.sh
@@ -7,7 +7,7 @@
 THIS_SCRIPT_DIR="$(dirname "$0")"
 MODS_DIR="${THIS_SCRIPT_DIR}/mods"
 
-mkdir ${MODS_DIR}
+mkdir -p ${MODS_DIR}
 
 echo "Mods destination directory: ${MODS_DIR}"
 echo " "
@@ -32,14 +32,19 @@ downlaod_file "https://edge.forgecdn.net/files/3110/694/DungeonCrawl-1.16.3-2.2.
 downlaod_file "https://edge.forgecdn.net/files/3110/386/elevatorid-1.16.4-1.7.8.jar"
 downlaod_file "https://edge.forgecdn.net/files/3038/811/ftb-backups-2.1.1.6.jar"
 downlaod_file "https://edge.forgecdn.net/files/3091/862/ftb-gui-library-1603.1.1.25.jar"
+downlaod_file "https://edge.forgecdn.net/files/3113/275/industrial-foregoing-1.16.4-3.2.2-daea863.jar"
 downlaod_file "https://edge.forgecdn.net/files/3100/510/Mekanism-1.16.4-10.0.17.444.jar"
 downlaod_file "https://edge.forgecdn.net/files/3100/512/MekanismGenerators-1.16.4-10.0.17.444.jar"
+downlaod_file "https://edge.forgecdn.net/files/3086/492/Patchouli-1.16.2-47.jar"
+downlaod_file "https://edge.forgecdn.net/files/3106/707/Psi 1.16-88.jar"
 downlaod_file "https://edge.forgecdn.net/files/3106/626/Quark-r2.4-275.jar"
 downlaod_file "https://edge.forgecdn.net/files/3088/871/QuarkOddities-1.16.3.jar"
 downlaod_file "https://edge.forgecdn.net/files/3104/34/rsgauges-1.16.4-1.2.6-b1.jar"
 downlaod_file "https://edge.forgecdn.net/files/3097/119/ScalableCatsForce-2.13.3-build-5-with-library.jar"
 downlaod_file "https://edge.forgecdn.net/files/3071/526/SimpleStorageNetwork-1.16.3-1.1.2.jar"
 downlaod_file "https://edge.forgecdn.net/files/3103/28/swingthroughgrass-1.16.4-1.5.3.jar"
+downlaod_file "https://edge.forgecdn.net/files/3100/160/The_Undergarden-1.16.3-0.3.8.jar"
+downlaod_file "https://edge.forgecdn.net/files/3113/257/titanium-1.16.4-3.2.1.jar"
 downlaod_file "https://edge.forgecdn.net/files/3086/49/ToolBelt-1.16.3-1.15.2.jar"
 downlaod_file "https://edge.forgecdn.net/files/3100/516/worldedit-forge-mc1.16.3-7.2.0-dist.jar"
 downlaod_file "https://edge.forgecdn.net/files/3031/390/xptome-1.16.2-v1.1.6.jar"
